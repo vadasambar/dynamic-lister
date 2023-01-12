2cffed3120efbd1144a1205382c2294786fc2f7f
vadasambar <surajrbanakar@gmail.com>
2023-01-12 11:34:57 +0530

    refactor: add example for both namespaced and cluster scoped resources - also add example for namespace limited and namespaced resource but not limited to namespace listers - better display - update README with new output - minor refactors and fixes

--- README.md
+++ README.md
@@ -2,16 +2,60 @@ This is an example for creating a `Lister` using dynamic client for resource of 
 
 ## How it works
 
-```
+All nodes:
+```bash
 $ kubectl get nodes
 NAME                            STATUS   ROLES                  AGE   VERSION
 k3d-dynamic-corefile-server-0   Ready    control-plane,master   21d   v1.24.4+k3s1
 
 ```
-
+Pods in all the namespaces:
+```bash
+$ kubectl get po -A 
+NAMESPACE     NAME                                      READY   STATUS      RESTARTS        AGE
+kube-system   helm-install-traefik-crd-vplbc            0/1     Completed   0               21d
+kube-system   helm-install-traefik-q26gv                0/1     Completed   1               21d
+kube-system   local-path-provisioner-7b7dc8d6f5-q8k6x   1/1     Running     37 (127m ago)   21d
+kube-system   coredns-b96499967-tmswr                   1/1     Running     37 (127m ago)   21d
+kube-system   traefik-7cd4fcff68-dt82t                  1/1     Running     37 (127m ago)   21d
+kube-system   svclb-traefik-98312b71-66crq              2/2     Running     92 (127m ago)   21d
+kube-system   metrics-server-668d979685-tbblc           1/1     Running     37 (127m ago)   21d
+default       nginx                                     1/1     Running     0               3m39s
 ```
+
+```bash
 $ go run main.go
-I0112 10:43:26.123920   77494 shared_informer.go:273] Waiting for caches to sync for node-lister
-I0112 10:43:26.224850   77494 shared_informer.go:280] Caches are synced for node-lister
-node k3d-dynamic-corefile-server-0
+I0112 11:32:42.385967  105474 shared_informer.go:273] Waiting for caches to sync for generic-nodes-lister
+I0112 11:32:42.486633  105474 shared_informer.go:280] Caches are synced for generic-nodes-lister
+
+All nodes:
+----------
+k3d-dynamic-corefile-server-0
+
+I0112 11:32:42.486738  105474 shared_informer.go:273] Waiting for caches to sync for generic-pods-lister
+I0112 11:32:42.587197  105474 shared_informer.go:280] Caches are synced for generic-pods-lister
+
+Pods in `kube-system` namespace:
+--------------------------------
+coredns-b96499967-tmswr
+traefik-7cd4fcff68-dt82t
+metrics-server-668d979685-tbblc
+helm-install-traefik-crd-vplbc
+helm-install-traefik-q26gv
+local-path-provisioner-7b7dc8d6f5-q8k6x
+svclb-traefik-98312b71-66crq
+
+I0112 11:32:42.587249  105474 shared_informer.go:273] Waiting for caches to sync for generic-pods-lister
+I0112 11:32:42.688035  105474 shared_informer.go:280] Caches are synced for generic-pods-lister
+
+Pods in all the namespaces:
+---------------------------
+traefik-7cd4fcff68-dt82t
+metrics-server-668d979685-tbblc
+nginx # NOTICE nginx POD HERE FROM THE default NAMESPACE
+helm-install-traefik-crd-vplbc
+helm-install-traefik-q26gv
+local-path-provisioner-7b7dc8d6f5-q8k6x
+svclb-traefik-98312b71-66crq
+coredns-b96499967-tmswr
 ```
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	apiv1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/labels"
@@ -18,11 +19,6 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/klog/v2"
 )
-
-// Note that for `core` group we use ""
-// Resource should be in plural form e.g., pods, deployments etc.,
-// Ref: https://github.com/kubernetes/client-go/issues/737
-var nodeGVR = schema.GroupVersionResource{Group: "", Version: "v1", Resource: "nodes"}
 
 func main() {
 
@@ -44,29 +40,93 @@ func main() {
 	// For stopping the reflector
 	stopCh := make(chan struct{})
 
-	nodeLister := NewNodeLister(dClient, nil, stopCh)
+	// Note that for `core` group we use ""
+	// Resource should be in plural form e.g., pods, deployments etc.,
+	// Ref: https://github.com/kubernetes/client-go/issues/737
+	var nodeGVR = schema.GroupVersionResource{Group: "", Version: "v1", Resource: "nodes"}
+
+	nodeLister := NewDynamicLister(dClient, stopCh, nodeGVR, apiv1.NamespaceAll)
 
 	// Get all the nodes
-	no, err := nodeLister.List(labels.Everything())
+	nos, err := nodeLister.List(labels.Everything())
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("node", no[0].GetName())
+	fmt.Println("") // add some space after the last line for better display
+	fmt.Println("All nodes:")
+	fmt.Println("----------")
+	for _, n := range nos {
+		fmt.Println(n.GetName())
+	}
+	fmt.Println("") // add some space after the last line for better display
+
+	// Note that for `core` group we use ""
+	// Resource should be in plural form e.g., pods, deployments etc.,
+	// Ref: https://github.com/kubernetes/client-go/issues/737
+	var podGVR = schema.GroupVersionResource{Group: "", Version: "v1", Resource: "pods"}
+
+	// Limit lister to a particular namespace (use only for namespaced resources)
+	podLister := NewDynamicLister(dClient, stopCh, podGVR, "kube-system")
+
+	// Get all the pods
+	po, err := podLister.List(labels.Everything())
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("") // add some space after the last line for better display
+	fmt.Println("Pods in `kube-system` namespace:")
+	fmt.Println("--------------------------------")
+	for _, p := range po {
+		fmt.Println(p.GetName())
+	}
+	fmt.Println("") // add some space after the last line for better display
+
+	// Limit lister to a particular namespace (use only for namespaced resources)
+	allPodsLister := NewDynamicLister(dClient, stopCh, podGVR, apiv1.NamespaceAll)
+
+	// Get all the pods in all the namespaces
+	allPo, err := allPodsLister.List(labels.Everything())
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("") // add some space after the last line for better display
+	fmt.Println("Pods in all the namespaces:")
+	fmt.Println("---------------------------")
+	for _, p := range allPo {
+		fmt.Println(p.GetName())
+	}
+	fmt.Println("") // add some space after the last line for better display
+
 }
 
-func NewNodeLister(dClient *dynamic.DynamicClient, filter func(*apiv1.Node) bool, stopChannel <-chan struct{}) dynamiclister.Lister {
+func NewDynamicLister(dClient *dynamic.DynamicClient, stopChannel <-chan struct{}, gvr schema.GroupVersionResource, namespace string) dynamiclister.Lister {
+
+	var lister func(ctx context.Context, opts metav1.ListOptions) (*unstructured.UnstructuredList, error)
+	var watcher func(ctx context.Context, opts metav1.ListOptions) (watch.Interface, error)
+
+	if namespace == apiv1.NamespaceAll {
+		lister = dClient.Resource(gvr).List
+		watcher = dClient.Resource(gvr).Watch
+	} else {
+		// For lister limited to a particular namespace
+		lister = dClient.Resource(gvr).Namespace(namespace).List
+		watcher = dClient.Resource(gvr).Namespace(namespace).Watch
+	}
+
 	// NewNamespaceKeyedIndexerAndReflector can be
 	// used for both namespace and cluster scoped resources
 	store, reflector := cache.NewNamespaceKeyedIndexerAndReflector(&cache.ListWatch{
 		ListFunc: func(options v1.ListOptions) (runtime.Object, error) {
-			return dClient.Resource(nodeGVR).List(context.Background(), options)
+			return lister(context.Background(), options)
 		},
 		WatchFunc: func(options v1.ListOptions) (watch.Interface, error) {
-			return dClient.Resource(nodeGVR).Watch(context.Background(), options)
+			return watcher(context.Background(), options)
 		},
 	}, unstructured.Unstructured{}, time.Hour)
-	nodeLister := dynamiclister.New(store, nodeGVR)
+	nodeLister := dynamiclister.New(store, gvr)
 
 	// Run reflector in the background so that we get new updates from the api-server
 	go reflector.Run(stopChannel)
@@ -76,13 +136,16 @@ func NewNodeLister(dClient *dynamic.DynamicClient, filter func(*apiv1.Node) bool
 	// Note: Based on the docs WaitForNamedCacheSync seems to be used to check if an informer has synced
 	// but the function is generic enough so we can use
 	// it for reflectors as well
-	cache.WaitForNamedCacheSync("node-lister", stopChannel, func() bool {
+	synced := cache.WaitForNamedCacheSync(fmt.Sprintf("generic-%s-lister", gvr.Resource), stopChannel, func() bool {
 		no, err := nodeLister.List(labels.Everything())
 		if err != nil {
 			klog.Error("err", err)
 		}
 		return len(no) > 0
 	})
+	if !synced {
+		klog.Error("couldn't sync cache")
+	}
 
 	return nodeLister
 }
