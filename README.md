c4b1fda1c13ebfa68ab3e70a4b0a1a6601f87eb3
vadasambar <surajrbanakar@gmail.com>
2023-01-12 10:45:29 +0530

    refactor: wait for sync instead of sleeping for 10s - add output in the README

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 This is an example for creating a `Lister` using dynamic client for resource of your choice. 
+
+## How it works
+
+```
+$ kubectl get nodes
+NAME                            STATUS   ROLES                  AGE   VERSION
+k3d-dynamic-corefile-server-0   Ready    control-plane,master   21d   v1.24.4+k3s1
+
+```
+
+```
+$ go run main.go
+I0112 10:43:26.123920   77494 shared_informer.go:273] Waiting for caches to sync for node-lister
+I0112 10:43:26.224850   77494 shared_informer.go:280] Caches are synced for node-lister
+node k3d-dynamic-corefile-server-0
+```
--- main.go
+++ main.go
@@ -16,17 +16,24 @@ import (
 	dynamiclister "k8s.io/client-go/dynamic/dynamiclister"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/klog/v2"
 )
 
+// Note that `core` group is set as empty
+// Resource should in plural form
+// Ref: https://github.com/kubernetes/client-go/issues/737
 var nodeGVR = schema.GroupVersionResource{Group: "", Version: "v1", Resource: "nodes"}
 
 func main() {
 
+	// Check and load kubeconfig from the path set
+	// in KUBECONFIG env variable (if not use default path of ~/.kube/config)
 	apiConfig, err := clientcmd.NewDefaultClientConfigLoadingRules().Load()
 	if err != nil {
 		panic(err)
 	}
 
+	// Create rest config from kubeconfig
 	restConfig, err := clientcmd.NewDefaultClientConfig(*apiConfig, &clientcmd.ConfigOverrides{}).ClientConfig()
 	if err != nil {
 		panic(err)
@@ -34,21 +41,23 @@ func main() {
 
 	dClient := dynamic.NewForConfigOrDie(restConfig)
 
+	// For stopping the reflector
 	stopCh := make(chan struct{})
+
 	nodeLister := NewNodeLister(dClient, nil, stopCh)
 
-	time.Sleep(time.Second * 10)
-
+	// Get all the nodes
 	no, err := nodeLister.List(labels.Everything())
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("no", *no[0])
-
+	fmt.Println("node", no[0].GetName())
 }
 
 func NewNodeLister(dClient *dynamic.DynamicClient, filter func(*apiv1.Node) bool, stopChannel <-chan struct{}) dynamiclister.Lister {
+	// NewNamespaceKeyedIndexerAndReflector doesn't
+	// mean it can be used for namespaced scope resources only
 	store, reflector := cache.NewNamespaceKeyedIndexerAndReflector(&cache.ListWatch{
 		ListFunc: func(options v1.ListOptions) (runtime.Object, error) {
 			return dClient.Resource(nodeGVR).List(context.Background(), options)
@@ -56,10 +65,21 @@ func NewNodeLister(dClient *dynamic.DynamicClient, filter func(*apiv1.Node) bool
 		WatchFunc: func(options v1.ListOptions) (watch.Interface, error) {
 			return dClient.Resource(nodeGVR).Watch(context.Background(), options)
 		},
-	}, unstructured.Unstructured{}, time.Second)
+	}, unstructured.Unstructured{}, time.Hour)
 	nodeLister := dynamiclister.New(store, nodeGVR)
 
+	// Run reflector in the background so that we get new updates from the api-server
 	go reflector.Run(stopChannel)
+
+	// Wait for reflector to sync the cache for the first time
+	// TODO: check if there's a better way to do this (listing all the nodes seems wasteful)
+	cache.WaitForNamedCacheSync("node-lister", stopChannel, func() bool {
+		no, err := nodeLister.List(labels.Everything())
+		if err != nil {
+			klog.Error("err", err)
+		}
+		return len(no) > 0
+	})
 
 	return nodeLister
 }
